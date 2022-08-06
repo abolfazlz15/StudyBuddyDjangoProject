c8f8cc6033505abbfd35024de472501a6a68e5ce
Abolfazl <abolfazlzaker1234@gmail.com>
2022-08-06 17:45:14 +0430

    add deletemessage, createroom and update home.html

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('createroom', views.CreateRoomView.as_view(), name='create-room'),
+
+    # Message URL
+    path('deletemesssge/<int:pk>', views.DeleteMessageView.as_view(), name='delete-message'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-from django.views.generic.base import TemplateView
+from django.views.generic import CreateView, TemplateView, View, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from .models import Message, Room, Topic
 
 
 class HomeView(TemplateView):
@@ -8,5 +9,31 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['test'] = 'abofazl'
+        context['rooms'] = Room.objects.filter(status=True)
+        context['topics'] = Topic.objects.all()
+        context['messages'] = Message.objects.filter(status=True)
+
+        # get number_of_rooms
+        number_of_rooms = 0
+        for i in context['topics']:
+            room = i.rooms.count()
+            number_of_rooms = number_of_rooms + room
+        context['count_all_room'] = number_of_rooms
         return context
+
+
+class CreateRoomView(CreateView):
+    model = Room
+    fields = ['topic', 'name', 'description']
+    template_name = 'core/create_room.html'
+    success_url = reverse_lazy('core:home')
+
+    def form_valid(self, form):
+        form.instance.host = self.request.user
+        return super().form_valid(form)
+
+
+class DeleteMessageView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('core:home')
+
