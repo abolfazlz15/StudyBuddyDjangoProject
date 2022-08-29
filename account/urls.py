ebe765538d433c7ccf1e5e7d001f5695ec9a9a6e
Abolfazl <abolfazlzaker1234@gmail.com>
2022-08-30 00:30:28 +0430

    add theme in gitignore and Login implementation

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'account'
 urlpatterns = [
     path('profile/<int:pk>', views.UserProfileView.as_view(), name='profile-detail'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 from django.views.generic import DetailView
+
+from .forms import LoginForm
 from .models import User
 
 
@@ -7,3 +12,15 @@ class UserProfileView(DetailView):
     model= User
     template_name = 'account/profile.html'
     
+
+
+class UserLoginView(generic.FormView):
+    template_name = 'account/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('core:home')
+        
+    def form_valid(self, form):
+        data = form.cleaned_data
+        user = User.objects.get(email=data['email'])
+        login(self.request, user)
+        return super().form_valid(form)
